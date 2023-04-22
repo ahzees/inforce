eaf4b6187d85d074d3078f25f5f62c3d4bbfa6b1
ahzees <matviy.stets@gmail.com>
2023-04-22 05:56:11 +0300

    chore: updated start.sh and manage.py [Task]: inforce-start.sh
    [Description list]
    refactor: updated start.sh and manage.py
    
    [Notes]
    <notes>

--- inforce/start.sh
+++ inforce/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+python inforce/manage.py makemigrations authentication
+python inforce/manage.py makemigrations restaurant
 python inforce/manage.py migrate authentication
 python inforce/manage.py migrate restaurant
 python inforce/manage.py migrate admin
